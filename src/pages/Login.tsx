abd49444684d342e66977c657d38dd8b7efa80c5
Andrii Kniaziev <andrii.kniaziev@iqvia.com>
2023-03-16 20:43:22 +0200

    Fix issue with google does not exist

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,11 +2,21 @@ import React, {useContext, useEffect} from 'react';
 import jwtDecode from "jwt-decode";
 import {AuthContext} from "../context/context";
 
+const loadScript = (src: string) =>
+    new Promise<void>((resolve, reject) => {
+        if (document.querySelector(`script[src="${src}"]`)) return resolve()
+        const script = document.createElement('script')
+        script.src = src
+        script.onload = () => resolve()
+        script.onerror = (err) => reject(err)
+        document.body.appendChild(script)
+    });
+
 const Login = () => {
     const {setGoogleUser} = useContext(AuthContext);
 
     const handleAuthResponse = (response: any) => {
-        console.log('Encoded JWT ID token: ' + response.credential);
+        console.log('Response: ' + response);
         const userObject = jwtDecode(response.credential);
         console.log(userObject);
         setGoogleUser(userObject);
@@ -14,17 +24,25 @@ const Login = () => {
     };
 
     useEffect(() => {
-        // @ts-ignore
-        google.accounts.id.initialize({
-            client_id: '275793562999-2ltbnuv5nlkf0vh80g6ir7joo06eqpft.apps.googleusercontent.com',
-            callback: handleAuthResponse,
-        });
+        const src = 'https://accounts.google.com/gsi/client'
+        const id = "275793562999-2ltbnuv5nlkf0vh80g6ir7joo06eqpft.apps.googleusercontent.com"
 
-        // @ts-ignore
-        google.accounts.id.renderButton(
-            document.getElementById('sign-in'),
-            {theme: 'outline', size: 'large'}
-        );
+        loadScript(src)
+            .then(() => {
+                // @ts-ignore
+                const _google = google;
+                console.log('Google: ')
+                console.log(_google)
+                _google.accounts.id.initialize({
+                    client_id: id,
+                    callback: handleAuthResponse,
+                })
+                _google.accounts.id.renderButton(
+                    document.getElementById('sign-in'),
+                    { theme: 'outline', size: 'large' }
+                )
+            })
+            .catch(console.error)
     }, []);
 
     return (
